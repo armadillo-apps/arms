22ebce01b1c6309b4724533caa5e4b46799e28d8
john <johnwee35@gmail.com>
2019-07-17 15:57:34 +0800

    edited config

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,9 +1,11 @@
-describe('My First Test', function () {
-  it('Does not do much!', function () {
-    expect(true).to.equal(true)
-  })
+describe('My First Test', function() {
+  it('Does not do much!', function() {
+    expect(true).to.equal(true);
+  });
 
-  it("should be able to visit the home page" , ()=> {
-      cy.visit("https://test-armadilo-arms.herokuapp.com")
-  })
-})
+  it('should be able to visit the home page', () => {
+    cy.visit('https://test-armadilo-arms.herokuapp.com');
+    const values = cy.get('.sideBar__headerContainer');
+    console.log(values);
+  });
+});
