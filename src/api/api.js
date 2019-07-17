b70009fa5d0514e56a8f271f3170a06388778f62
kaihengann <annkaiheng@gmail.com>
2019-07-17 16:55:06 +0800

    Kai-GS: Add createNewOccupant api call

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
-  baseURL: 'https://my-json-server.typicode.com/armadillo-apps/api'
+  baseURL: "https://my-json-server.typicode.com/armadillo-apps/api"
 });
+
+export const createNewOccupant = async (name, employeeId, remarks) => {
+  const requestBody = {
+    name: name,
+    employeeId: employeeId,
+    remarks: remarks
+  };
+  const response = await axios.post(
+    `${process.env.REACT_APP_URL}/occupants`,
+    requestBody
+  );
+  return response.data;
+};
