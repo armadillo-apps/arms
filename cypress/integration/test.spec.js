de6897da2175de8ff3fa08a3e5e9cb3f9bcc8f65
john <johnwee35@gmail.com>
2019-07-17 15:51:43 +0800

    jon updated cypgress

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -2,4 +2,8 @@ describe('My First Test', function () {
   it('Does not do much!', function () {
     expect(true).to.equal(true)
   })
+
+  it("should be able to visit the home page" , ()=> {
+      cy.visit("https://test-armadilo-arms.herokuapp.com")
+  })
 })
--- src/components/ApartmentDetail/ApartmentDetail.test.js
+++ src/components/ApartmentDetail/ApartmentDetail.test.js
@@ -1,34 +1,3 @@
-// import React from 'react';
-// // import { render, fireEvent, getByText,cleanup}  from '@testing-library/react';
-// // // import ApartmentDetail from './ApartmentDetail';
-// it('should ', () => {
-
-// });
-
-// describe('ApartmentDetail', () => {
-//   let mock;
-//   beforeEach(() => {
-//     mock = {
-//       name: 'China Square Central 01-01',
-//       capacity: 1,
-//       leases: [
-//         {
-//           leaseStart: '25 Jun 19',
-//           leaseEnd: '25 jun 22',
-//           monthlyRent: 5000
-//         }
-//       ]
-//     };
-//   });
-
-//   afterEach(cleanup);
-
-//   it('should show the details correctly when rendered', () => {
-//     // const { getByText } = render(<ApartmentDetail {...mock} />)
-//     // console.log(getByText('China Square Central 01-01'));
-//   });
-// });
-
 // Please don't remove until you have at least ONE passing test
 describe('Sample Test', () => {
   it('should do something', () => {
