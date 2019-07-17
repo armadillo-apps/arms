b8b629d24b3ded9020c70a3a38cdb5cc5c23169d
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-07-17 14:52:53 +0800

    Add fetchOccupant to data

--- src/service/data.js
+++ src/service/data.js
@@ -1,6 +1,11 @@
-import api from "../api/api";
+import api from '../api/api';
 
 export const fetchApartments = async () => {
-  const response = await api.get("/apartments");
+  const response = await api.get('/apartments');
+  return response.data;
+};
+
+export const fetchOccupants = async () => {
+  const response = await api.get('/occupants');
   return response.data;
 };
