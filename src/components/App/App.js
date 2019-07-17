0ff69f515270ac2b34f0a2ad86ee9d501ff835c4
gs195 <timedilation01@gmail.com>
2019-07-17 14:42:03 +0800

    GS-KAI: Convert App from functional to class component, add onChange handler

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import SideBar from "../SideBar/SideBar";
@@ -6,19 +6,37 @@ import Apartment from "../Apartment/Apartment";
 import Occupant from "../Occupant/Occupant";
 import NewOccupantForm from "../NewOccupantForm/NewOccupantForm";
 
-function App() {
-  return (
-    <section className="app">
-      <Router>
-        <SideBar />
-        <Switch>
-          <Route component={Apartment} exact path="/" />
-          <Route component={Occupant} exact path="/occupants" />
-          <Route component={NewOccupantForm} exact path="/newOccupant" />
-        </Switch>
-      </Router>
-    </section>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  onChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <section className="app">
+        <Router>
+          <SideBar />
+          <Switch>
+            <Route component={Apartment} exact path="/" />
+            <Route component={Occupant} exact path="/occupants" />
+            <Route
+              render={() => <NewOccupantForm onChange={this.onChange} />}
+              exact
+              path="/newOccupant"
+            />
+          </Switch>
+        </Router>
+      </section>
+    );
+  }
 }
 
 export default App;
