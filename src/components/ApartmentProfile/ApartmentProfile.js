52f10a51e5fefc6ece5e0bf66874177c756b6252
kaihengann <annkaiheng@gmail.com>
2019-07-18 19:57:46 +0800

    Kai: Fix failing unit test

--- src/components/ApartmentProfile/ApartmentProfile.js
+++ src/components/ApartmentProfile/ApartmentProfile.js
@@ -11,51 +11,53 @@ const ApartmentProfile = props => {
       return apartment._id === apartmentId;
     });
     return (
-      <div className="apartmentProfile">
-        <Link className="sideBar__heading" to="/">
-          &lt; Back
-        </Link>
-        <h1 className="apartmentProfile__heading">{filteredApartment[0].name}</h1>
-        <div className="apartmentProfile__generalDetails">
-          <h2>Address</h2>
-          <p>{filteredApartment[0].address}</p>
-          <h2>Bedrooms</h2>
-          <p>{filteredApartment[0].bedrooms}</p>
-        </div>
-        <div className="apartmentProfile__vacancyDetails">
-          <h2>Capacity</h2>
-          <p>{filteredApartment[0].capacity}</p>
-          <h2>Occupants</h2>
-        </div>
+      <div className="apartmentProfileContainer">
+        <div className="apartmentProfile">
+          <Link className="apartmentProfile__backButton" to="/">
+            &lt; Back
+          </Link>
+          <h1 className="apartmentProfile__heading">{filteredApartment[0].name}</h1>
+          <div className="apartmentProfile__generalDetails">
+            <h2>Address</h2>
+            <p>{filteredApartment[0].address}</p>
+            <h2>Bedrooms</h2>
+            <p>{filteredApartment[0].bedrooms}</p>
+          </div>
+          <div className="apartmentProfile__vacancyDetails">
+            <h2>Capacity</h2>
+            <p>{filteredApartment[0].capacity}</p>
+            <h2>Occupants</h2>
+          </div>
 
-        <table className="apartmentProfile__occupants">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Check-In</th>
-              <th>Check-Out</th>
-            </tr>
-          </thead>
-        </table>
-        <h2>Leases</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Lease Start</th>
-              <th>Lease End</th>
-              <th>Monthly Rent</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredApartment[0].leases.length > 0 ? (
-              filteredApartment[0].leases.map(lease => (
-                <Lease key={lease._id} leaseInfo={lease} />
-              ))
-            ) : (
-              <tr>No Leases yet!</tr>
-            )}
-          </tbody>
-        </table>
+          <table className="apartmentProfile__occupants">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Check-In</th>
+                <th>Check-Out</th>
+              </tr>
+            </thead>
+          </table>
+          <h2>Leases</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Lease Start</th>
+                <th>Lease End</th>
+                <th>Monthly Rent</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredApartment[0].leases.length > 0 ? (
+                filteredApartment[0].leases.map(lease => (
+                  <Lease key={lease._id} leaseInfo={lease} />
+                ))
+              ) : (
+                <tr>No Leases yet!</tr>
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
