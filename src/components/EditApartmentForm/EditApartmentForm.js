cbc369be686f73b82b18ba2d97295ac3fddc773f
kaihengann <annkaiheng@gmail.com>
2019-07-31 18:16:33 +0800

    Kai-Jess: Add onChange and onSubmit to EditApartmentForm

--- src/components/EditApartmentForm/EditApartmentForm.js
+++ src/components/EditApartmentForm/EditApartmentForm.js
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Input from "../Input/Input";
 
-const EditApartmentForm = ({ apartment }) => {
-  const {
-    name,
-    address,
-    bedrooms,
-    capacity,
-    country,
-    landlord: { name: landlordName, accountNumber },
-    remarks
-  } = apartment;
+const EditApartmentForm = ({ apartment, closeModal, onSubmit }) => {
+  const [name, setName] = useState(apartment.name);
+  const [address, setAddress] = useState(apartment.address);
+  const [bedrooms, setBedrooms] = useState(apartment.bedrooms);
+  const [capacity, setCapacity] = useState(apartment.capacity);
+  const [country, setCountry] = useState(apartment.country);
+  const [landlordName, setLandlordName] = useState(apartment.landlord.name);
+  const [remarks, setRemarks] = useState(apartment.remarks);
+  const [accountNumber, setAccountNumber] = useState(
+    apartment.landlord.accountNumber
+  );
 
   return (
     <form
       className="editApartmentFormContainer"
       data-testid="editApartmentForm"
+      onSubmit={(event) => onSubmit(event, {name})}
     >
       <h1 className="editApartmentForm__heading">Edit Apartment</h1>
       <div className="editApartmentForm">
@@ -25,14 +27,18 @@ const EditApartmentForm = ({ apartment }) => {
           label="Apartment Name"
           name="name"
           type="text"
-          defaultValue={name}
+          value={name}
+          onChange={event => setName(event.target.value)}
         />
         <Input
           id="address"
           label="Address"
           name="address"
           type="text"
-          defaultValue={address}
+          value={address}
+          onChange={event => {
+            setAddress(event.target.value);
+          }}
         />
         <Input
           id="bedrooms"
@@ -40,7 +46,10 @@ const EditApartmentForm = ({ apartment }) => {
           name="bedrooms"
           type="number"
           min="0"
-          defaultValue={bedrooms}
+          value={bedrooms}
+          onChange={event => {
+            setBedrooms(event.target.value);
+          }}
         />
         <Input
           id="capacity"
@@ -48,28 +57,40 @@ const EditApartmentForm = ({ apartment }) => {
           name="capacity"
           type="number"
           min="0"
-          defaultValue={capacity}
+          value={capacity}
+          onChange={event => {
+            setCapacity(event.target.value);
+          }}
         />
         <Input
           id="country"
           label="Country"
           name="country"
           type="text"
-          defaultValue={country}
+          value={country}
+          onChange={event => {
+            setCountry(event.target.value);
+          }}
         />
         <Input
           id="landlordName"
           label="Landlord Name"
           name="landlordName"
           type="text"
-          defaultValue={landlordName}
+          value={landlordName}
+          onChange={event => {
+            setLandlordName(event.target.value);
+          }}
         />
         <Input
           id="landlordAccountNumber"
           label="Landlord A/C Number"
           name="landlordAccountNumber"
           type="text"
-          defaultValue={accountNumber}
+          value={accountNumber}
+          onChange={event => {
+            setAccountNumber(event.target.value);
+          }}
         />
         <label htmlFor="remarks">Remarks</label>
         <textarea
@@ -77,10 +98,13 @@ const EditApartmentForm = ({ apartment }) => {
           name="remarks"
           rows="3"
           cols="30"
-          defaultValue={remarks}
+          value={remarks}
+          onChange={event => {
+            setRemarks(event.target.value);
+          }}
         />
       </div>
-      <input type="button" value="Cancel" />
+      <input type="button" value="Cancel" onClick={closeModal} />
       <input
         className="editApartmentForm__updateButton"
         value="Update"
@@ -95,7 +119,7 @@ EditApartmentForm.propTypes = {
 };
 
 EditApartmentForm.defaultProps = {
-  apartment: { landlord: {} }
+  apartment: { name: "", landlord: {} }
 };
 
 export default EditApartmentForm;
