38cef1827df0b6b4911a2bc23c432278e88b5c0a
hojunle <hojunle@outlook.com>
2019-10-31 14:44:51 +0800

    Melvin-Lishan: added login input fields

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,9 +1,43 @@
 import React, { Component } from "react";
 import "./LoginForm.css";
+import Input from "../Input/Input";
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleEmailChange = () => {};
+
+  handlePasswordChange = () => {};
+
   render() {
-    return <div>Hello User</div>;
+    return (
+      <div>
+        <Input
+          id="email"
+          label="Email"
+          name="email"
+          type="text"
+          value={this.state.email}
+          onChange={this.handleEmailChange}
+          required
+        />
+        <Input
+          id="password"
+          label="Password"
+          name="password"
+          type="password"
+          value={this.state.password}
+          onChange={this.handlePasswordChange}
+          required
+        />
+      </div>
+    );
   }
 }
 
--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render, getByText } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import LoginForm from "./LoginForm";
 
 describe("Login Form", () => {
-  it("should display the login form", () => {
-    const { getByText } = render(<LoginForm />);
-    expect(getByText("Hello User")).toBeInTheDocument();
+  it("should display the email and password input", () => {
+    const { getByLabelText } = render(<LoginForm />);
+    expect(getByLabelText("Email")).toBeInTheDocument();
+    expect(getByLabelText("Password")).toBeInTheDocument();
   });
 });
