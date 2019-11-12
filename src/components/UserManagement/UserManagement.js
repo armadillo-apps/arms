30dde50f4403c5e13338f264819d675d81332169
ShafoYoisho <muhammadsyafizuraimi@Gmail.com>
2019-11-12 17:36:06 +0800

    Syafi-Yun #14: Add test for UserManagement Page

---
+++ src/components/UserManagement/UserManagement.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+// import SearchBar from "../SearchBar/SearchBar";
+import "./UserManagement.css";
+import "../ApartmentProfile/ApartmentProfile.css";
+// import { removeUser } from "../../api/api";
+
+class UserManagement extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      data: props.users
+    };
+  }
+
+  componentDidMount() {
+    console.log(this.state.data);
+  }
+
+  deleteUser = id => {};
+
+  render() {
+    return (
+      <div>
+        <table className="occupants__table" cellSpacing="0" cellPadding="0">
+          <thead className="occupants__header2">
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>ID</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map(user => {
+              return (
+                <tr key={user._id}>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.role}</td>
+                  <td>{user._id}</td>
+                  <td>
+                    <button onClick={() => {}}>Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default UserManagement;
---
+++ src/components/UserManagement/UserManagement.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import UserManagement from "./UserManagement";
+import { render, fireEvent, getByText } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/react/cleanup-after-each";
+
+const users = [
+  {
+    name: "Bob",
+    email: "bob@thoughtworks.com",
+    role: "admin",
+    _id: "1234"
+  },
+  {
+    name: "Ann",
+    email: "ann@thoughtworks.com",
+    role: "manager",
+    _id: "12345"
+  }
+];
+
+describe("User Management Page", () => {
+  it("should render name of user", () => {
+    const { getByText } = render(<UserManagement users={users} />);
+    expect(getByText("Name")).toBeInTheDocument();
+    expect(getByText("Bob")).toBeInTheDocument();
+  });
+});
