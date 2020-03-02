c66ef7b9e8363f4f75e87804857f99b820bfd53a
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-03-02 14:22:37 +0800

    [#96] refactor: new apartment forms to hooks

--- src/components/NewApartmentForm/NewApartmentForm.js
+++ src/components/NewApartmentForm/NewApartmentForm.js
@@ -1,70 +1,68 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Input from "../Input/Input";
 import TextArea from "../Input/TextArea";
 import "./NewApartmentForm.css";
 import ConfirmationMessage from "../ConfirmationMessage/ConfirmationMessage";
 import { createNewApartment } from "../../api/api";
 
-class NewApartmentForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      address: "",
-      bedrooms: 1,
-      capacity: 1,
-      leaseStart: "",
-      leaseEnd: "",
-      rent: "",
-      currency: "",
-      landLordName: "",
-      landLordAccount: "",
-      country: "",
-      remarks: "",
-      success: false,
-      message: "",
-      submitted: false,
-      status: ""
-    };
-  }
-
-  onFormChange = event => {
-    const { name, value } = event.target;
-    const newName = name.split("")[0].toLowerCase() + name.substring(1);
-    this.setState({
-      [newName]: value
-    });
+const NewApartmentForm = props => {
+  const emptyForm = {
+    name: "",
+    address: "",
+    bedrooms: 1,
+    capacity: 1,
+    leaseStart: "",
+    leaseEnd: "",
+    rent: "",
+    currency: "",
+    landLordName: "",
+    landLordAccount: "",
+    country: "",
+    remarks: "",
+    status: ""
   };
 
-  onFormSubmit = async event => {
+  const [formInputs, setFormInputs] = useState(emptyForm);
+  const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const onFormChange = event => {
+    const { name, value } = event.target;
+    const newName = name.split("")[0].toLowerCase() + name.substring(1);
+    setFormInputs({ ...formInputs, [newName]: value });
+  };
+
+  const onFormSubmit = async event => {
     try {
       event.preventDefault();
       const data = {
-        name: this.state.name,
-        address: this.state.address,
-        bedrooms: this.state.bedrooms,
-        capacity: this.state.capacity,
-        status: this.state.status,
+        name: formInputs.name,
+        address: formInputs.address,
+        bedrooms: formInputs.bedrooms,
+        capacity: formInputs.capacity,
+        status: formInputs.status,
         leases: [
           {
-            leaseStart: this.state.leaseStart,
-            leaseEnd: this.state.leaseEnd,
-            monthlyRent: this.state.rent,
-            currency: this.state.currency
+            leaseStart: formInputs.leaseStart,
+            leaseEnd: formInputs.leaseEnd,
+            monthlyRent: formInputs.rent,
+            currency: formInputs.currency
           }
         ],
         landlord: {
-          name: this.state.landLordName,
-          accountNumber: this.state.landLordAccount,
-          mobile: this.state.landLordMobile,
-          email: this.state.landLordEmail
+          name: formInputs.landLordName,
+          accountNumber: formInputs.landLordAccount,
+          mobile: formInputs.landLordMobile,
+          email: formInputs.landLordEmail
         },
-        country: this.state.country,
-        remarks: this.state.remarks
+        country: formInputs.country,
+        remarks: formInputs.remarks
       };
+
       const output = await createNewApartment(data);
 
-      this.setState({
+      const clearedForm = {
         name: "",
         address: "",
         bedrooms: 1,
@@ -77,206 +75,201 @@ class NewApartmentForm extends Component {
         landLordAccount: "",
         country: "",
         remarks: "",
-        success: true,
-        message: output,
-        submitted: true,
         status: ""
-      });
-      this.props.triggerRender();
-      this.props.history.push(`/apartments`);
+      };
+
+      setMessage(output);
+      setSuccess(true);
+      setSubmitted(true);
+      setFormInputs(clearedForm);
+      props.triggerRender();
+      props.history.push(`/apartments`);
     } catch (err) {
-      this.setState({
-        success: false,
-        message: "Unable to create new apartment :(",
-        submitted: true
-      });
+      setMessage("Unable to create new apartment :(");
+      setSuccess(false);
+      setSubmitted(true);
     }
   };
 
-  render() {
-    return (
-      <form className="apartmentForm" onSubmit={this.onFormSubmit}>
-        <div className="apartmentForm__div">
-          <h1 className="apartmentForm__heading">Create New Apartment</h1>
+  return (
+    <form className="apartmentForm" onSubmit={onFormSubmit}>
+      <div className="apartmentForm__div">
+        <h1 className="apartmentForm__heading">Create New Apartment</h1>
+        <Input
+          id="apartment-name"
+          label="Apartment name*"
+          name="Name"
+          onChange={onFormChange}
+          value={formInputs.name}
+          type="text"
+          required
+        />
+        <Input
+          id="address"
+          label="Address*"
+          name="Address"
+          onChange={onFormChange}
+          value={formInputs.address}
+          type="text"
+          required
+        />
+        <div className="formDivide">
           <Input
-            id="apartment-name"
-            label="Apartment name*"
-            name="Name"
-            onChange={this.onFormChange}
-            value={this.state.name}
+            id="landlord-name"
+            label="Landlord name*"
+            name="LandLordName"
+            onChange={onFormChange}
+            value={formInputs.landLordName}
             type="text"
+            width="20rem"
             required
           />
           <Input
-            id="address"
-            label="Address*"
-            name="Address"
-            onChange={this.onFormChange}
-            value={this.state.address}
+            id="landlord-account-number"
+            label="Landlord A/C number*"
+            name="LandLordAccount"
+            onChange={onFormChange}
+            value={formInputs.landLordAccount}
             type="text"
+            width="20rem"
             required
-          />
-          <div className="formDivide">
-            <Input
-              id="landlord-name"
-              label="Landlord name*"
-              name="LandLordName"
-              onChange={this.onFormChange}
-              value={this.state.landLordName}
-              type="text"
-              width="20rem"
-              required
-            />
-            <Input
-              id="landlord-account-number"
-              label="Landlord A/C number*"
-              name="LandLordAccount"
-              onChange={this.onFormChange}
-              value={this.state.landLordAccount}
-              type="text"
-              width="20rem"
-              required
-            />
-          </div>
-          <div className="formDivide">
-            <Input
-              id="lease-start"
-              label="Lease start*"
-              name="LeaseStart"
-              onChange={this.onFormChange}
-              value={this.state.leaseStart}
-              type="date"
-              width="20rem"
-              required
-            />
-            <Input
-              id="lease-end"
-              label="Lease end*"
-              name="LeaseEnd"
-              onChange={this.onFormChange}
-              value={this.state.leaseEnd}
-              type="date"
-              min={this.state.leaseStart}
-              width="20rem"
-              required
-            />
-          </div>
-
-          <div className="formDivide">
-            <section>
-              <Input
-                id="rental-per-month"
-                label="Rental per month*"
-                name="Rent"
-                onChange={this.onFormChange}
-                value={this.state.rent}
-                type="number"
-                min="0"
-                width="20rem"
-                required
-              />
-            </section>
-            <section>
-              <label htmlFor="currency" className="apartmentForm__currency">
-                Currency*
-              </label>
-              <select
-                id="currency"
-                name="Currency"
-                required
-                onChange={this.onFormChange}
-                value={this.state.currency}
-                width="20rem"
-              >
-                <option value="">Select currency</option>
-                <option value="SGD">SGD</option>
-                <option value="THB">THB</option>
-              </select>
-            </section>
-          </div>
-
-          <div className="formDivide">
-            <Input
-              id="capacity"
-              label="Capacity*"
-              name="Capacity"
-              onChange={this.onFormChange}
-              value={this.state.capacity}
-              type="number"
-              min="0"
-              width="61px"
-              required
-            />
-            <Input
-              id="bedrooms"
-              label="Bedrooms*"
-              name="Bedrooms"
-              onChange={this.onFormChange}
-              value={this.state.bedrooms}
-              type="number"
-              min="0"
-              width="61px"
-              required
-            />
-            <section>
-              <label htmlFor="country" className="apartmentForm__country">
-                Country*
-              </label>
-              <select
-                id="country"
-                name="Country"
-                required
-                value={this.state.country}
-                onChange={this.onFormChange}
-              >
-                <option value="">Select country</option>
-                <option value="Singapore">Singapore</option>
-                <option value="Thailand">Thailand</option>
-              </select>
-            </section>
-            <section>
-              <label htmlFor="status" className="apartmentForm__status">
-                Apartment Status*
-              </label>
-              <select
-                id="status"
-                name="status"
-                required
-                value={this.state.status}
-                onChange={this.onFormChange}
-              >
-                <option value="">Select status</option>
-                <option value="Active">Active</option>
-                <option value="Inactive">Inactive</option>
-              </select>
-            </section>
-          </div>
-          <TextArea
-            id="remarks"
-            label="Remarks"
-            name="Remarks"
-            className="apartmentForm__remarks"
-            onChange={this.onFormChange}
-            value={this.state.remarks}
-            type="text"
-          />
-          {this.state.submitted ? (
-            <ConfirmationMessage
-              message={this.state.message}
-              success={this.state.success}
-            />
-          ) : (
-            ""
-          )}
-          <input
-            className="apartmentForm__createButton"
-            value="Create"
-            type="submit"
           />
         </div>
-      </form>
-    );
-  }
-}
+        <div className="formDivide">
+          <Input
+            id="lease-start"
+            label="Lease start*"
+            name="LeaseStart"
+            onChange={onFormChange}
+            value={formInputs.leaseStart}
+            type="date"
+            width="20rem"
+            required
+          />
+          <Input
+            id="lease-end"
+            label="Lease end*"
+            name="LeaseEnd"
+            onChange={onFormChange}
+            value={formInputs.leaseEnd}
+            type="date"
+            min={formInputs.leaseStart}
+            width="20rem"
+            required
+          />
+        </div>
+
+        <div className="formDivide">
+          <section>
+            <Input
+              id="rental-per-month"
+              label="Rental per month*"
+              name="Rent"
+              onChange={onFormChange}
+              value={formInputs.rent}
+              type="number"
+              min="0"
+              width="20rem"
+              required
+            />
+          </section>
+          <section>
+            <label htmlFor="currency" className="apartmentForm__currency">
+              Currency*
+            </label>
+            <select
+              id="currency"
+              name="Currency"
+              required
+              onChange={onFormChange}
+              value={formInputs.currency}
+              width="20rem"
+            >
+              <option value="">Select currency</option>
+              <option value="SGD">SGD</option>
+              <option value="THB">THB</option>
+            </select>
+          </section>
+        </div>
+
+        <div className="formDivide">
+          <Input
+            id="capacity"
+            label="Capacity*"
+            name="Capacity"
+            onChange={onFormChange}
+            value={formInputs.capacity}
+            type="number"
+            min="0"
+            width="61px"
+            required
+          />
+          <Input
+            id="bedrooms"
+            label="Bedrooms*"
+            name="Bedrooms"
+            onChange={onFormChange}
+            value={formInputs.bedrooms}
+            type="number"
+            min="0"
+            width="61px"
+            required
+          />
+          <section>
+            <label htmlFor="country" className="apartmentForm__country">
+              Country*
+            </label>
+            <select
+              id="country"
+              name="Country"
+              required
+              value={formInputs.country}
+              onChange={onFormChange}
+            >
+              <option value="">Select country</option>
+              <option value="Singapore">Singapore</option>
+              <option value="Thailand">Thailand</option>
+            </select>
+          </section>
+          <section>
+            <label htmlFor="status" className="apartmentForm__status">
+              Apartment Status*
+            </label>
+            <select
+              id="status"
+              name="status"
+              required
+              value={formInputs.status}
+              onChange={onFormChange}
+            >
+              <option value="">Select status</option>
+              <option value="Active">Active</option>
+              <option value="Inactive">Inactive</option>
+            </select>
+          </section>
+        </div>
+        <TextArea
+          id="remarks"
+          label="Remarks"
+          name="Remarks"
+          className="apartmentForm__remarks"
+          onChange={onFormChange}
+          value={formInputs.remarks}
+          type="text"
+        />
+        {submitted ? (
+          <ConfirmationMessage message={message} success={success} />
+        ) : (
+          ""
+        )}
+        <input
+          className="apartmentForm__createButton"
+          value="Create"
+          type="submit"
+        />
+      </div>
+    </form>
+  );
+};
 
 export default NewApartmentForm;
