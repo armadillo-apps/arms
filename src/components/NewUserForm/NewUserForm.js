bc10894f0f2102ec7f11be843ac748729e7be629
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-03-03 11:48:31 +0800

    [#98] refactor: use hooks for new user form

--- src/components/NewUserForm/NewUserForm.js
+++ src/components/NewUserForm/NewUserForm.js
@@ -1,119 +1,110 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./NewUserForm.css";
 import Input from "../Input/Input";
 import { createNewUser } from "../../api/api";
 import ConfirmationMessage from "../ConfirmationMessage/ConfirmationMessage";
 
-class NewUserForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      password: "",
-      role: "",
-      success: false,
-      message: "",
-      submitted: false
-    };
-  }
-  onFormChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
+const NewUserForm = props => {
+  const emptyForm = {
+    name: "",
+    email: "",
+    password: "",
+    role: ""
   };
 
-  onFormSubmit = async event => {
+  const [formInputs, setFormInputs] = useState(emptyForm);
+  const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const onFormChange = event => {
+    const { name, value } = event.target;
+    setFormInputs({ ...formInputs, [name]: value });
+  };
+
+  const onFormSubmit = async event => {
     try {
       event.preventDefault();
-      const { name, email, password, role } = this.state;
-      await createNewUser(name, email, password, role);
-      this.setState({
-        name: "",
-        email: "",
-        password: "",
-        role: "",
-        success: true,
-        message: "Success",
-        submitted: true
-      });
-      this.props.triggerRender();
-      //   this.props.history.push(`/occupants`);
+
+      await createNewUser(
+        formInputs.name,
+        formInputs.email,
+        formInputs.password,
+        formInputs.role
+      );
+
+      setMessage("Success");
+      setSuccess(true);
+      setSubmitted(true);
+
+      props.triggerRender();
     } catch (err) {
-      this.setState({
-        success: false,
-        message: "Unable to create new user :(",
-        submitted: true
-      });
+      setMessage("Unable to create new user :(");
+      setSuccess(false);
+      setSubmitted(true);
     }
   };
 
-  render() {
-    return (
-      <form className="newUserFormContainer" onSubmit={this.onFormSubmit}>
-        <h1 className="newUserForm__heading">Create New User</h1>
-        <div className="newUserForm">
-          <Input
-            id="name"
-            label="Name*"
-            name="name"
-            onChange={this.onFormChange}
-            value={this.state.name}
-            type="text"
-            required
-          />
-          <Input
-            id="email"
-            label="Email*"
-            name="email"
-            onChange={this.onFormChange}
-            value={this.state.email}
-            type="text"
-            required
-          />
-          <Input
-            id="password"
-            label="Password*"
-            name="password"
-            onChange={this.onFormChange}
-            value={this.state.password}
-            type="text"
-            required
-          />
-          <section>
-            <label htmlFor="role" className="userForm__roleLabel">
-              User Role
-            </label>
-            <select
-              id="role"
-              name="role"
-              onChange={this.onFormChange}
-              value={this.state.role}
-              type="text"
-            >
-              <option value="">Select Role...</option>
-              <option value="admin">Admin</option>
-              <option value="manager">Manager</option>
-            </select>
-          </section>
-        </div>
-        {this.state.submitted ? (
-          <ConfirmationMessage
-            success={this.state.success}
-            message={this.state.message}
-          />
-        ) : (
-          ""
-        )}
-        <input
-          className="occupantForm__createButton"
-          value="Create"
-          type="submit"
+  return (
+    <form className="newUserFormContainer" onSubmit={onFormSubmit}>
+      <h1 className="newUserForm__heading">Create New User</h1>
+      <div className="newUserForm">
+        <Input
+          id="name"
+          label="Name*"
+          name="name"
+          onChange={onFormChange}
+          value={formInputs.name}
+          type="text"
+          required
         />
-      </form>
-    );
-  }
-}
+        <Input
+          id="email"
+          label="Email*"
+          name="email"
+          onChange={onFormChange}
+          value={formInputs.email}
+          type="text"
+          required
+        />
+        <Input
+          id="password"
+          label="Password*"
+          name="password"
+          onChange={onFormChange}
+          value={formInputs.password}
+          type="text"
+          required
+        />
+        <section>
+          <label htmlFor="role" className="userForm__roleLabel">
+            User Role
+          </label>
+          <select
+            id="role"
+            name="role"
+            onChange={onFormChange}
+            value={formInputs.role}
+            type="text"
+          >
+            <option value="">Select Role...</option>
+            <option value="admin">Admin</option>
+            <option value="manager">Manager</option>
+          </select>
+        </section>
+      </div>
+      {submitted ? (
+        <ConfirmationMessage success={success} message={message} />
+      ) : (
+        ""
+      )}
+      <input
+        className="occupantForm__createButton"
+        value="Create"
+        type="submit"
+      />
+    </form>
+  );
+};
 
 export default NewUserForm;
