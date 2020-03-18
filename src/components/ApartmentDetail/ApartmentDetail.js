c572f4ac81dab006b3b6eed69267d6cd54eca6e5
Kai Heng <kaiheng.ann@thoughtworks.com>
2020-03-18 15:44:01 +0800

    [TechDebt] refactor: rename variables, simplify class names and add tests for lease expiry

--- src/components/ApartmentDetail/ApartmentDetail.js
+++ src/components/ApartmentDetail/ApartmentDetail.js
@@ -1,24 +1,25 @@
 import React from "react";
+import moment from "moment";
+
 import "./ApartmentDetail.css";
 import extractDate from "../../utils/ExtractDate";
 import { sgdFormatter, thbFormatter } from "../../utils/formatMoney";
-import moment from "moment";
 
 const ApartmentDetail = ({ status, vacancy, name, leases, _id, history }) => {
-  const [firstLeases] = leases;
-  const { leaseStart, leaseEnd, monthlyRent, currency } = firstLeases;
+  const [firstLease] = leases;
+
+  const { leaseStart, leaseEnd, monthlyRent, currency } = firstLease;
 
   const leaseEndDate = extractDate(leaseEnd);
+
   const monthBeforeLeaseEnd = moment(new Date(leaseEndDate))
     .subtract(1, "months")
     .format("YYYY-MM-DD");
 
-  const leaseEndAlertBoolean = moment(new Date()).isBetween(
+  const isLeaseExpiring = moment(new Date(Date.now())).isBetween(
     monthBeforeLeaseEnd,
     new Date(leaseEndDate)
   );
-
-  const leaseEndAlert = boolean => (boolean ? "leaseEndAlert" : "");
 
   const monthlyRentFormatted = monthlyRent => {
     if (currency === "SGD") {
@@ -35,20 +36,15 @@ const ApartmentDetail = ({ status, vacancy, name, leases, _id, history }) => {
         history.push(`/apartments/${_id}`);
       }}
     >
-      <td
-        className={`apartmentDetails__td ${status} ${leaseEndAlert(
-          leaseEndAlertBoolean
-        )}`}
-      >
-        {status}
-      </td>
+      <td className={`apartmentDetails__td ${status}`}>{status}</td>
       <td className={vacancy <= 0 ? "inverted" : "positive"}>{vacancy}</td>
       <td className="apartmentDetails__td">{name}</td>
       <td className="apartmentDetails__td">{extractDate(leaseStart)}</td>
       <td
-        className={`apartmentDetails__td ${leaseEndAlert(
-          leaseEndAlertBoolean
-        )}`}
+        className={`apartmentDetails__td ${
+          isLeaseExpiring ? "leaseExpiring" : ""
+        }`}
+        data-testid="leaseEndDate"
       >
         {leaseEndDate}
       </td>
--- src/components/ApartmentDetail/ApartmentDetail.test.js
+++ src/components/ApartmentDetail/ApartmentDetail.test.js
@@ -2,13 +2,35 @@ import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render } from "@testing-library/react";
 import "@testing-library/react/cleanup-after-each";
+
 import { Apartment } from "../Apartment/Apartment";
 import { fireEvent } from "@testing-library/react/dist";
 import * as data from "../../api/api";
 
 const mockPost = jest.spyOn(data, "fetchApartments");
+const testDate = new Date("2020-05-26T11:00:00.135Z").valueOf();
+jest.spyOn(global.Date, "now").mockReturnValue(testDate);
 
 const apartments = [
+  {
+    _id: "123",
+    name: "The Forest",
+    address: "18 Bogus Street #01-01",
+    bedrooms: 1,
+    capacity: 10,
+    status: "Active",
+    leases: [
+      {
+        leaseStart: "25 June 2019",
+        leaseEnd: "24 June 2021",
+        monthlyRent: 5000,
+        currency: "THB"
+      }
+    ]
+  }
+];
+
+const expiringApartments = [
   {
     _id: "123",
     name: "The Forest",
@@ -49,5 +71,25 @@ describe("Redirect to apartment profile page", () => {
     await Promise.resolve();
 
     expect(history.push).toHaveBeenCalled();
+  });
+});
+
+describe("Lease expiry logic", () => {
+  it("should show Lease End in red colour when lease is expiring in 1 month", () => {
+    const { getByTestId } = render(
+      <Apartment apartments={expiringApartments} stays={[]} history={history} />
+    );
+    const leaseEndDate = getByTestId("leaseEndDate");
+
+    expect(leaseEndDate).toHaveClass("leaseExpiring");
+  });
+
+  it("should not show Lease End in red colour when lease is not expiring in 1 month", () => {
+    const { getByTestId } = render(
+      <Apartment apartments={apartments} stays={[]} history={history} />
+    );
+    const leaseEndDate = getByTestId("leaseEndDate");
+
+    expect(leaseEndDate).not.toHaveClass("leaseExpiring");
   });
 });
