7e459e199ecaa70aeea68ceb8372651cbc9cc1bf
kaiNext <kaiheng.ann@thoughtworks.com>
2020-06-15 23:53:47 +0800

    chore: modify pre-push-hook script to exit upon any error

--- githooks/pre-push-hook.sh
+++ githooks/pre-push-hook.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 
 stash_name=$(date "+%Y%m%d_%H%M%S")'-pre-push-stash'
-if [[ -n $(git status -s) ]] ; then
-    git stash push -u -m $stash_name
-    echo "Pushing local changes to git stash"
+if [[ -n $(git status -s) ]]; then
+  git stash push -u -m $stash_name
+  echo "Pushing local changes to git stash"
 fi
 
-test_return_code=0
-if ! npm run test:cov ; then
-    test_return_code=1
-    echo "Unit test failed"
+if ! npm run test:cov; then
+  echo "Unit test failed"
+  exit 1
 fi
 
-if ! npm run cy:dev ; then
-    test_return_code=1
-    echo "UI test failed"
+if ! npm run cy:dev; then
+  echo "UI test failed"
+  exit 1
 fi
 
 stash_id=$(git stash list | grep $stash_name | cut -d: -f1)
-if [[ -n $stash_id ]] ; then
-    git stash pop $stash_id
-    echo "Popped pre-push stash from git stash"
+if [[ -n $stash_id ]]; then
+  git stash pop $stash_id
+  echo "Popped pre-push stash from git stash"
 fi
 
-exit ${test_return_code}
+exit 0
