6527ae8b16645532266198c3978c81c5998ee72f
kaiNext <kaiheng.ann@thoughtworks.com>
2020-06-17 08:52:41 +0800

    [#123] fix: change searchbar selector to fix UI test

--- cypress/integration/apartments.spec.js
+++ cypress/integration/apartments.spec.js
@@ -129,7 +129,7 @@ describe("Apartments, Occupant, and ApartmentAssign", () => {
       cy.get("select[name=homeOffice]").select("Australia, Melbourne");
       cy.get("select[name=status]").select(status);
       cy.get("input[type=submit]").click();
-      cy.get("input[type=text]").type(modName);
+      cy.get("[data-testid=Occupant__searchBar]").type(modName);
       cy.get("td")
         .contains(modName)
         .click();
