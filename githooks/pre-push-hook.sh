668c9c58dee2e109592f53631220a65f370fac19
kaiNext <kaiheng.ann@thoughtworks.com>
2020-07-12 11:24:57 +0800

    [Tech] fix: update pre-push hook to pop stash upon failure

--- githooks/pre-push-hook.sh
+++ githooks/pre-push-hook.sh
@@ -1,25 +1,34 @@
 #!/usr/bin/env bash
 
-stash_name=$(date "+%Y%m%d_%H%M%S")'-pre-push-stash'
-if [[ -n $(git status -s) ]]; then
-  git stash push -u -m $stash_name
-  echo "Pushing local changes to git stash"
-fi
+function stashUncommittedChanges() {
+  stash_name=$(date "+%Y%m%d_%H%M%S")'-pre-push-stash'
+  if [[ -n $(git status -s) ]]; then
+    git stash push -u -m $stash_name
+    echo "Pushing local changes to git stash"
+  fi
+}
+
+function popUncommittedChanges() {
+  stash_id=$(git stash list | grep $stash_name | cut -d: -f1)
+  if [[ -n $stash_id ]]; then
+    git stash pop $stash_id
+    echo "Popped pre-push stash from git stash"
+  fi
+}
+
+stashUncommittedChanges
 
 if ! npm run test:cov; then
   echo "Unit test failed"
+  popUncommittedChanges
   exit 1
 fi
 
 if ! npm run cy:dev; then
   echo "UI test failed"
+  popUncommittedChanges
   exit 1
 fi
 
-stash_id=$(git stash list | grep $stash_name | cut -d: -f1)
-if [[ -n $stash_id ]]; then
-  git stash pop $stash_id
-  echo "Popped pre-push stash from git stash"
-fi
-
+popUncommittedChanges
 exit 0
