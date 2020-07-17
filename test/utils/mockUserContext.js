50a8fee5b7455250f8d61e66a8e04b31f4b4ad9c
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-07-17 23:03:36 +0800

    [#132] fix: mock dispatch in user context

--- test/utils/mockUserContext.js
+++ test/utils/mockUserContext.js
@@ -2,6 +2,7 @@ import * as UserContext from "../../src/context/UserContext";
 
 export const mockUserContext = user => {
   jest.spyOn(UserContext, "useUserContext").mockImplementation(() => ({
-    state: user
+    state: user,
+    dispatch: jest.fn()
   }));
 };
