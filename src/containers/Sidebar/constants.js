41864e1c21d851f73e00ceede7ca3db3d5461928
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-07-19 16:24:17 +0800

    [#132] refactor: reuse pathname constant

--- src/containers/Sidebar/constants.js
+++ src/containers/Sidebar/constants.js
@@ -1,22 +1,23 @@
 import ApartmentsLogo from "../../assets/apartments.svg";
 import OccupantsLogo from "../../assets/occupants.svg";
 import ChangePasswordLogo from "../../assets/edit.svg";
+import routes from "../../router/RouterPaths";
 
 export const sidebarLinks = [
   {
-    pathname: "/apartments",
+    pathname: routes.APARTMENTS,
     text: "Apartments",
     imgUrl: ApartmentsLogo,
     imgAlt: "apartments"
   },
   {
-    pathname: "/occupants",
+    pathname: routes.OCCUPANTS,
     text: "Occupants",
     imgUrl: OccupantsLogo,
     imgAlt: "occupants"
   },
   {
-    pathname: "/changePassword",
+    pathname: routes.CHANGE_PASSWORD,
     text: "Change Password",
     imgUrl: ChangePasswordLogo,
     imgAlt: "change password"
